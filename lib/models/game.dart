21827ae7ae400970e299c88f5980dbca403aca63
Shinya Iwasaki <sinproject.develop@gmail.com>
2021-02-01 16:29:09 +0900

    gameStore

---
+++ lib/models/game.dart
@@ -0,0 +1,8 @@
+class Game {
+  String mode;
+  int count;
+  int time;
+  bool inPlay;
+
+  Game({this.mode, this.count, this.inPlay = false, this.time});
+}
---
+++ lib/stores/game.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:renda_clone/models/game.dart';
+import 'package:renda_clone/util/var/index.dart';
+
+class GameStore extends ChangeNotifier {
+  // 実際に管理される商品のリスト
+  Game _game =
+      Game(mode: gameModes["A"], time: gameTimes["A"], inPlay: false, count: 0);
+  // 外側から直接変更されないように、getterのみ公開
+  Game get game => _game;
+
+  changeGameMode(String value) {
+    if (game.mode == value) {
+      return;
+    }
+    game.mode = value;
+    notifyListeners();
+  }
+
+  incrementCount() => {game.count += 1, notifyListeners()};
+  decrementTime() {
+    while (game.count >= 0) {
+      game.time -= 1;
+      notifyListeners();
+    }
+    notifyListeners();
+    return;
+  }
+
+  inPlayToggle() => {game.inPlay = !game.inPlay, notifyListeners()};
+
+  gameStart() {
+    inPlayToggle();
+    decrementTime();
+    inPlayToggle();
+  }
+}
